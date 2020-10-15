b069c2d1442f5e0503166884b7856abafc3cb8b1
tetsuyainfra <tetsuyainfra@gmail.com>
2020-10-15 10:17:21 +0900

    new template

---
+++ rails-templates/apply_cancancan.rb
@@ -0,0 +1,6 @@
+# apply_cancancan.rb
+def source_paths
+  [__dir__]
+end
+
+apply "#{__dir__}/apply_devise.rb"
--- samples_template.sh
+++ samples_template.sh
@@ -2,3 +2,4 @@
 
 #rails new Sample -m devise-template/create-user.rb
 rails new DeviseSample -m rails-templates/apply_devise.rb --skip-javascript
+rails new CanSample    -m rails-templates/apply_cancancan.rb --skip-javascript
