1d35b990f3a1d0f511ba2f7b281a942491134db5
tetsuyainfra <tetsuyainfra@gmail.com>
2020-10-15 10:49:52 +0900

    fix

--- samples_template.sh
+++ samples_template.sh
@@ -2,4 +2,5 @@
 
 #rails new Sample -m devise-template/create-user.rb
 rails new DeviseSample -m rails-templates/apply_devise.rb --skip-javascript
-rails new CanSample    -m rails-templates/apply_cancancan.rb --skip-javascript
+rails new DeviseSample -m rails-templates/apply_simple_blog.rb --skip-javascript
+# rails new CanSample    -m rails-templates/apply_cancancan.rb --skip-javascript
